8464fd6f1754dae4c2009614cf28ee4bae6ed62e
kat-1356 <khyatitrip@gmail.com>
2025-05-20 09:34:30 +0530

    Create book.js

---
+++ book.js
@@ -0,0 +1,14 @@
+ const books = [
+    { title: "The Hobbit", author: "J.R.R. Tolkien", alreadyRead: true },
+    { title: "The Lord of the Rings", author: "J.R.R. Tolkien", alreadyRead: false },
+    { title: "1984", author: "George Orwell", alreadyRead: true }
+];
+
+books.forEach(book => {
+    const bookInfo = `"${book.title}" by ${book.author}`;
+    if (book.alreadyRead) {
+        console.log(`You already read ${bookInfo}`);
+    } else {
+        console.log(`You still need to read ${bookInfo}`);
+    }
+});
